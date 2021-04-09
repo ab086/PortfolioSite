8fd9eb7d6c7a6c6f7189f88486010fccb7b8d7db
Austin Blair <austinb@cart32.com>
2021-04-09 14:32:24 -0500

    Began work on content

---
+++ gnx.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php include "includes/top.php"; ?>
+
+    <title>Document</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    
+</head>
+<body>
+    
+    <div class="container-fluid">
+        <div class="row">
+            <?php include "includes/navbar.php"; ?>
+
+            <div class="col-md-12 text-center my-5">
+                <h1 class="gnx-header">Gulf Northern Xpress</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <img src="images/gnx-home.png" alt="GNX Home Page" class="img-fluid">
+            </div>
+
+            <div class="col-md-6">
+                <h3><a href="https://gulfnorthernxpress.com/" target="_blank">Gulf Northern Xpress</a> is a trucking company located within the Midwestern United States. They have never had a website before until I built one for them!<br>The main focus of the website is for hiring, thus I created them forms that will allow emails to be sent to hiring managers. The forms also implement security, so scripting attacks can't happen.<br><strong>This project took about 1 month to complete.</strong></h3>
+            </div>
+        </div>
+    </div>
+
+    <?php include "includes/bottom.php"; ?>
+</body>
+</html>
